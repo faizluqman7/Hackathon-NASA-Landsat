d08395b480e5a27a563ff61928acd0f63c63de74
faizluqman7 <faizluqman7@gmail.com>
2024-10-06 13:52:21 +0100

    node js server deployeddd

--- client/script.js
+++ client/script.js
@@ -288,7 +288,7 @@ function handleLocationError(browserHasGeolocation, pos) {
 		const lng = document.getElementById('lng').value;
 
 		// Send data to backend for sending the email
-		fetch('http://localhost:3000/send-email', {
+		fetch('https://hackathon-nasa-landsat.onrender.com/send-email', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
